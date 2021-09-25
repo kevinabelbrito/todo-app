fbfac9d947c17e0116d3418f5777f4e7deec6c79
Kevin Brito <kevinabelbrito@hotmail.com>
2021-09-25 12:59:13 -0400

    Implements axios with vue-axios library

---
+++ src/axios/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+//axios.defaults.baseURL = 'http://localhost:8000/api/'
+axios.defaults.headers = {
+    'Accept': 'application/json'
+}
+
+export default axios
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import { createApp } from 'vue'
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fas } from "@fortawesome/free-solid-svg-icons";
+import axios from './axios'
+import vueAxios from 'vue-axios'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -11,6 +13,7 @@ library.add(fas)
 
 createApp(App)
     .use(router)
+    .use(vueAxios, axios)
     .use(store)
     .component('fa', FontAwesomeIcon)
     .mount('#app')
