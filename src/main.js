d17510adb3c43ccc3e829f0a2fae2761b76013a2
Kevin Brito <kevinabelbrito@hotmail.com>
2021-09-24 21:13:59 -0400

    Implements fontawesome

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import { createApp } from 'vue'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './index.css'
 
+library.add(fas)
+
 createApp(App)
     .use(router)
     .use(store)
+    .component('fa', FontAwesomeIcon)
     .mount('#app')
