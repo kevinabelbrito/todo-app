ccb9e5dd967ab9a5c788f8849460fb0dcce67a39
Kevin Brito <kevinabelbrito@hotmail.com>
2021-09-25 13:26:51 -0400

    Put title and illustration on dashboard page

--- src/components/panel/Dashboard.vue
+++ src/components/panel/Dashboard.vue
@@ -1,5 +1,10 @@
 <template>
-    <div>
-        <h1>Here's the dashboard</h1>
+    <div class="flex flex-auto justify-center items-center">
+        <div class="max-w-lg">
+            <h2 class="text-center text-4xl font-normal">
+                Bienvenido <span class="font-bold">Jones</span>
+            </h2>
+            <img src="@assets/img/illustration.svg" class="mx-auto">
+        </div>
     </div>
 </template>
